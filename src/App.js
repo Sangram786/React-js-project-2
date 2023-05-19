7fd88f7fe3f1bdad9c0ac2ed5aa2e7b4abd8d2be
Sangram <sangramkesharichoudhury6@gmail.com>
2023-05-19 14:47:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/cart" element={<Cart/>}></Route>
+        </>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
